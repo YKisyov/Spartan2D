03fc763e49795af689e80a1ae94854483d885f86
YKisyov <javacodex@gmail.com>
2023-02-12 15:15:26 +0200

    Added some boulderings of the main object and a custom color to it.

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -8,7 +8,8 @@ import java.awt.*;
 
 public class GamePanel extends JPanel {
     private MouseInput mouseInput;
-    private int xDelta = 100, yDelta = 100; //init position;
+    private float xDelta = 100, yDelta = 100; //init position;
+    private float xDirection = 0.09f, yDirection = 0.09f;
     private int frames;
     private long lastCheckedForNewFrameAt = 0L;
 
@@ -22,15 +23,28 @@ public class GamePanel extends JPanel {
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g); // it is clearing surface and allowing us to paint again;
-        g.fillRect(xDelta, yDelta, 200, 50);
+        updateRectangle();
+
+        g.setColor(new Color(150, 20, 90));
+        g.fillRect((int) xDelta, (int) yDelta, 200, 50);
         frames++;
 
         if (System.currentTimeMillis() - lastCheckedForNewFrameAt >= 1000) {
             lastCheckedForNewFrameAt = System.currentTimeMillis();
             System.out.println("FPS: " + frames);
-            frames =  0;
+            frames = 0;
         }
         repaint();
+    }
+
+    private void updateRectangle() {
+        xDelta += xDirection;
+        if (xDelta > 400 || xDelta < 0) {
+            xDirection *= -1;
+        }
+        yDelta += yDirection;
+        if (yDelta > 400 || yDelta < 0)
+            yDirection *= -1;
     }
 
 
